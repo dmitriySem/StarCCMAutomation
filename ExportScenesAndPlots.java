708f8fa37ec59ffd9aa5453c822dfa91630fd0d6
Dmitriy Semenov <dsemenov@saec.ru>
2024-02-04 13:40:23 +0500

    Добфвил графический интерфейс

--- ExportScenesAndPlots.java
+++ ExportScenesAndPlots.java
@@ -1,8 +1,25 @@
 import star.base.neo.DoubleVector;
+import star.base.report.ExpressionReport;
+import star.base.report.SumReport;
 import star.common.*;
+import star.coremodule.objectselector.ModelDescriptor;
+import star.coremodule.objectselector.ObjectSelectorField;
+import star.coremodule.objectselector.SelectorDescriptor;
+import star.coremodule.objectselector.SelectorUtils;
+import star.coremodule.ui.MessageDialog;
+import star.energy.TotalTemperatureProfile;
+import star.flow.MassFlowAverageReport;
+import star.flow.MassFlowRateProfile;
+import star.radiation.common.RadiationTemperatureProfile;
+import star.species.MassFractionProfile;
 import star.vis.*;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.File;
-import java.util.Scanner; 
+import java.util.Collection;
 
 public class ExportScenesAndPlots extends StarMacro {
 	public Simulation sim;
@@ -10,21 +27,117 @@ public class ExportScenesAndPlots extends StarMacro {
     public void execute() {
 
 		sim = getActiveSimulation();
+
+		JFrame frame = new JFrame("Steady state exporter");
+		frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		frame.setLayout(new GridBagLayout());
+
+		//Create and set up the content pane.
+		MyContentPane myContentPane = new MyContentPane();
+		frame.setContentPane(myContentPane);
+
+		//Display the window.
+		frame.pack();
+		frame.setLocationRelativeTo(null);
+		frame.setMinimumSize(frame.getSize());
+		if (System.getProperty("os.name").contains("Linux")) {
+			frame.setAlwaysOnTop(true);
+		}
+		frame.setVisible(true);
         
-        String dir = sim.getSessionDir(); //get the name of the simulation's directory
-        String sep = System.getProperty("file.separator"); //get the right separator for your operative system
-	  File resultFolder = new File(dir + "\\" + sim + "_Results");
-	  resultFolder.mkdir();
 
-	  File sceneFolder = new File(resultFolder + "\\Scenes");
-	  sceneFolder.mkdir();
+    }
 
-	  File plotFolder = new File(resultFolder + "\\Plots");
-	  plotFolder.mkdir();
-              
+    private SimpleAnnotation createAnnotation(String str, double rang){
+
+		SimpleAnnotation simpleAnnotation =
+				sim.getAnnotationManager().createAnnotation(SimpleAnnotation.class);
+
+		simpleAnnotation.setPresentationName(str);
+
+		simpleAnnotation.setText(String.format("%s: %.1f", str, rang));
+
+		return simpleAnnotation;
+
+
+
+	}
+
+	private class MyContentPane extends JPanel implements ActionListener {
+
+		protected ModelDescriptor modelDescriptorScenes;
+		protected ObjectSelectorField sceneSelector;
+		protected Collection<Scene> exportScenes;
+
+		public MyContentPane() {
+
+			this.setLayout(new GridBagLayout());
+			GridBagConstraints c = new GridBagConstraints();
+			c.insets = new Insets(3, 3, 3, 3);
+			c.weightx = 1;
+
+			// Boundary selection panel
+			c.gridx = 0;
+			c.gridy = 0;
+			c.fill = GridBagConstraints.HORIZONTAL;
+			JPanel panel1 = new JPanel();
+			panel1.setLayout(new GridBagLayout());
+			panel1.setBorder(BorderFactory.createTitledBorder("Select scenes"));
+
+			// Predefined selection controls for boundaries
+			c.fill = GridBagConstraints.HORIZONTAL;
+			panel1.add(new JLabel("Scenes:"), c);
+
+			modelDescriptorScenes = new SelectorDescriptor.Builder(sim,
+					FilterModel.SceneFilterModel).build();
+			sceneSelector = new ObjectSelectorField(modelDescriptorScenes);
+			sceneSelector.setPreferredSize(new Dimension(300, 25));
+			c.gridy = 1;
+			panel1.add(sceneSelector, c);
+
+			// A button to display selected boundaries in scene
+			c.fill = GridBagConstraints.NONE;
+			c.anchor = GridBagConstraints.LINE_START;
+			c.gridx = 1;
+			JButton sceneButton = new JButton("Export");
+			sceneButton.setActionCommand("export");
+			sceneButton.addActionListener(this);
+			sceneButton.setToolTipText("export scenes");
+			panel1.add(sceneButton, c);
+
+			c.anchor = GridBagConstraints.LINE_START;
+			c.gridx = 0;
+			c.gridy = 0;
+			c.gridwidth = 2;
+			this.add(panel1, c);
+		}
+
+		public void actionPerformed(ActionEvent e) {
+			if ("export".equals(e.getActionCommand())) {
+				if (!sceneSelector.hasSelection()) {
+					MessageDialog warning = new MessageDialog("No scenes selected");
+					warning.show();
+				} else {
+					exportScenes = SelectorUtils.createObjects(sim, sceneSelector.getSelected());
+					RunExportScenes(exportScenes);
+				}
+			}
+		}
+
+		public void RunExportScenes(Collection<Scene> sceneCollection){
+		String dir = sim.getSessionDir(); //get the name of the simulation's directory
+		String sep = System.getProperty("file.separator"); //get the right separator for your operative system
+		File resultFolder = new File(dir + "\\" + sim + "_Results");
+	  	resultFolder.mkdir();
+
+		File sceneFolder = new File(resultFolder + "\\Scenes");
+	  	sceneFolder.mkdir();
+
+		File plotFolder = new File(resultFolder + "\\Plots");
+	  	plotFolder.mkdir();
+
 
         for (Scene scn: sim.getSceneManager().getScenes()) {
-
 
 			LogoAnnotation logoAnnotation =
 					((LogoAnnotation) sim.getAnnotationManager().getObject("Logo"));
@@ -46,29 +159,14 @@ public class ExportScenesAndPlots extends StarMacro {
 			simpleAnnotationProp_min.setPosition(new DoubleVector(new double[] {0.35, 0.0, 0.0}));
 
 			sim.println("Saving Scene: " + scn.getPresentationName());
-	      	scn.printAndWait(resolvePath(sceneFolder + sep + scn.getPresentationName() + ".jpg"), 1, 1920, 1080);
-		
-	   	
-        }
+			scn.printAndWait(resolvePath(sceneFolder + sep + scn.getPresentationName() + ".jpg"), 1, 1920, 1080);
+
+		}
 
 //	  for (StarPlot plt : sim.getPlotManager().getObjects()) {
 //	      sim.println("Saving Plot: " + plt.getPresentationName());
 //	      plt.encode(resolvePath(plotFolder + sep + plt.getPresentationName() + ".jpg"), "jpg", 2560, 1440);
 //	  }
-    }
-
-    private SimpleAnnotation createAnnotation(String str, double rang){
-
-		SimpleAnnotation simpleAnnotation =
-				sim.getAnnotationManager().createAnnotation(SimpleAnnotation.class);
-
-		simpleAnnotation.setPresentationName(str);
-
-		simpleAnnotation.setText(String.format("%s: %.1f", str, rang));
-
-		return simpleAnnotation;
-
-
-
+	}
 	}
 }
